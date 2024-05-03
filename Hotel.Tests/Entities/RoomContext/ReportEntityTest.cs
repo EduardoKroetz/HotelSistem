5fdad545856e422513158f8656ef24fef2744f11
Eduardo Kroetz <eduardokroetz0@gmail.com>
2024-05-03 15:31:04 -0300

    criar testes para as entidades Category e Report

---
+++ Hotel.Tests/Entities/RoomContext/ReportEntityTest.cs
@@ -0,0 +1,26 @@
+using Hotel.Domain.Entities.RoomContext.ReportEntity;
+using Hotel.Domain.Exceptions;
+
+namespace Hotel.Tests.Entities.RoomContext;
+
+[TestClass]
+public class ReportEntityTest
+{
+  [TestMethod]
+  public void ValidCategory_MustBeValid()
+  {
+    var report = new Report("Vazamento no cano da pia","Vazamento no cano da pia do quarto 123",Domain.Enums.EPriority.High,TestParameters.Employee,"Consertar");
+    Assert.AreEqual(true,report.IsValid);
+  }
+
+  [TestMethod]
+  [ExpectedException(typeof(ValidationException))]
+  [DataRow("","")]
+  [DataRow("Sumario",TestParameters.DescriptionMaxCaracteres)]
+  [DataRow(TestParameters.DescriptionMaxCaracteres,"Descrição")]
+  public void InvalidReportParameters_ExpectedException(string summary,string description)
+  {
+    new Report(summary,description,Domain.Enums.EPriority.High,TestParameters.Employee,"Consertar");
+    Assert.Fail();
+  }
+}
