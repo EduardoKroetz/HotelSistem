04ef03f67d4d618d33c4adb14505810971186212
Eduardo Kroetz <eduardokroetz0@gmail.com>
2024-05-03 15:00:04 -0300

    criar testes para os ValueObjects

---
+++ Hotel.Tests/ValueObjects/NameTest.cs
@@ -0,0 +1,18 @@
+using Hotel.Domain.Exceptions;
+using Hotel.Domain.ValueObjects;
+
+namespace Hotel.Tests.ValueObjects;
+
+[TestClass]
+public class NameTest
+{
+  [TestMethod]
+  [ExpectedException(typeof(ValidationException))]
+  [DataRow("","")]
+  public void InvalidParameters_ExpectedException(string firstName, string lastName)
+  {
+    new Name(firstName, lastName); 
+    Assert.Fail();
+  }
+
+}
