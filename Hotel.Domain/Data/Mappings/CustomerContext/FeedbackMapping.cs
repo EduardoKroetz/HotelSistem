3c8c40e77715f8bfd9858c3dcd5cb184a2018ff2
Eduardo Kroetz <eduardokroetz0@gmail.com>
2024-05-03 23:31:37 -0300

    Criar mapeamentos para o contexto de Clientes(CustomerContext). -Criar Mapeamento para entidade Customer(CustomerMapping.cs); -Criar Mapeamento para entidade Feedback(FeedbackMapping.cs).

---
+++ Hotel.Domain/Data/Mappings/CustomerContext/FeedbackMapping.cs
@@ -0,0 +1,56 @@
+using Hotel.Domain.Data.Mappings.Base;
+using Hotel.Domain.Entities.CustomerContext.FeedbackEntity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Hotel.Domain.Data.Mappings.CustomerContext;
+
+public class FeedbackMapping : EntityBaseMapping<Feedback> ,IEntityTypeConfiguration<Feedback>
+{
+  public void Configure(EntityTypeBuilder<Feedback> builder)
+  {
+    BaseMapping(builder);
+
+    builder.ToTable("Feedbacks");
+
+    builder.Property(f => f.Comment)
+      .IsRequired()
+      .HasColumnType("VARCHAR")
+      .HasMaxLength(255);
+
+    builder.Property(f => f.Rate)
+      .IsRequired()
+      .HasColumnType("SMALLINT");
+
+    builder.Property(f => f.Likes)
+      .IsRequired()
+      .HasColumnType("SMALLINT");;
+
+    builder.Property(f => f.Deslikes)
+      .IsRequired()
+      .HasColumnType("SMALLINT");;
+
+    builder.Property(f => f.UpdatedAt)
+      .IsRequired()
+      .HasColumnType("DATETIME");
+
+    builder.HasOne(f => f.Customer)
+      .WithMany(c => c.Feedbacks)
+      .HasForeignKey(f => f.CustomerId)
+      .IsRequired()
+      .OnDelete(DeleteBehavior.SetNull);
+
+    builder.HasOne(f => f.Reservation)
+      .WithMany()
+      .HasForeignKey(f => f.ReservationId)
+      .IsRequired()
+      .OnDelete(DeleteBehavior.SetNull);
+
+    builder.HasOne(f => f.Room)
+      .WithMany()
+      .HasForeignKey(f => f.RoomId)
+      .IsRequired() 
+      .OnDelete(DeleteBehavior.SetNull);
+  }
+}
+
