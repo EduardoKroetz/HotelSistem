8e42a10d0958e6def4c9e8507a244ddfaa6506b1
Eduardo Kroetz <eduardokroetz0@gmail.com>
2024-05-04 11:12:12 -0300

    Mapear as tabelas do contexto de funcionários(EmployeeContext) -Mapear a tabela de funcionários(EmployeeMapping.cs); -Mapear a tabela de responsabilidades de funcionários(ResponsabilityMapping.cs).

---
+++ Hotel.Domain/Data/Mappings/EmployeeContext/EmployeeMapping.cs
@@ -0,0 +1,41 @@
+using Hotel.Domain.Data.Mappings.Base;
+using Hotel.Domain.Entities.EmployeeContext.EmployeeEntity;
+using Hotel.Domain.Entities.EmployeeContext.ResponsabilityEntity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace Hotel.Domain.Data.Mappings.EmployeeContext;
+public class EmployeeMapping : UserBaseMapping<Employee>, IEntityTypeConfiguration<Employee>
+{
+  public void Configure(EntityTypeBuilder<Employee> builder)
+  {
+    BaseMapping(builder);
+
+    builder.ToTable("Employees");
+
+    builder.Property(e => e.Salary)
+      .HasColumnType("DECIMAL(18,2)"); 
+
+    builder.HasMany(e => e.Responsabilities)
+      .WithMany()
+      .UsingEntity<Dictionary<string,object>>
+      (
+        "EmployeeResponsabilities",
+        j => j
+          .HasOne<Responsability>()
+          .WithMany()
+          .HasForeignKey("ResponsabilityId")
+          .HasConstraintName("FK_EmployeeResponsabilities_Responsability")
+          .OnDelete(DeleteBehavior.SetNull),
+        j => j
+          .HasOne<Employee>()
+          .WithMany()
+          .HasForeignKey("EmployeeId")
+          .HasConstraintName("FK_EmployeeResponsabilities_Employee")
+          .OnDelete(DeleteBehavior.SetNull)
+      );
+      
+  }
+}
+
---
+++ Hotel.Domain/Data/Mappings/EmployeeContext/ResponsabilityMapping.cs
@@ -0,0 +1,31 @@
+using Hotel.Domain.Data.Mappings.Base;
+using Hotel.Domain.Entities.EmployeeContext.ResponsabilityEntity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Hotel.Domain.Data.Mappings.ResponsabilityContext;
+public class ResponsabilityMapping : EntityBaseMapping<Responsability>, IEntityTypeConfiguration<Responsability>
+{
+  public void Configure(EntityTypeBuilder<Responsability> builder)
+  {
+    BaseMapping(builder);
+
+    builder.ToTable("Responsabilities");
+
+    builder.Property(p => p.Name)
+      .IsRequired()
+      .HasColumnType("VARCHAR")
+      .HasMaxLength(100);
+
+    builder.Property(p => p.Description)
+      .IsRequired()
+      .HasColumnType("VARCHAR")
+      .HasMaxLength(255);
+
+    builder.Property(p => p.Priority)
+      .IsRequired()
+      .HasConversion<int>();
+
+  }
+}
+
