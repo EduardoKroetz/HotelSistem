5f5cb1b4ef037a87d25866dff30e51e114738001
Eduardo Kroetz <eduardokroetz0@gmail.com>
2024-05-12 16:13:46 -0300

    Criar um filtro de extensão -Método do filtro de extensão para usar com propriedades que tem um operador -Método do filro de extensão para aplicar parâmetros 'skip','take' e 'createdAt' na query

--- Hotel.Domain/Extensions/QueryUtilities.cs
+++ Hotel.Domain/Extensions/QueryUtilities.cs
@@ -1,41 +1,76 @@
-﻿using Hotel.Domain.Entities.Base;
+﻿using Hotel.Domain.DTOs.Base;
+using Hotel.Domain.Entities.Base;
+using System.Linq.Expressions;
 
 namespace Hotel.Domain.Extensions;
 public static class QueryUtility
 {
-  public static IQueryable<T> FilterCreatedAtOperator<T>(this IQueryable<T> query, string filterOperator, DateTime createdAt)
+  public static IQueryable<T> FilterByOperator<T,TProperty>(this IQueryable<T> query, string? filterOperator, Expression<Func<T,TProperty>> propertySelector,TProperty value)
       where T : Entity
   {
-    switch (filterOperator.ToLower())
+    if (filterOperator == null)
+      return query;
+   
+    if (value == null)
     {
-      case "gt": // Greater than
-      query = query.Where(x => x.CreatedAt.Date > createdAt.Date);
-      break;
-      case "lt": // Less than
-      query = query.Where(x => x.CreatedAt.Date < createdAt.Date);
-      break;
-      case "eq": // Equal to
-      query = query.Where(x => x.CreatedAt.Date == createdAt.Date);
-      break;
+      switch (filterOperator.ToLower())
+      {
+        case "asc": //Ascending
+        return query.OrderBy(
+          Expression.Lambda<Func<T, object>>(
+            propertySelector.Body,
+            propertySelector.Parameters[0])
+        );
+        case "desc": //Descending
+        return query.OrderByDescending(
+          Expression.Lambda<Func<T, object>>(
+            propertySelector.Body,
+            propertySelector.Parameters[0])
+        );
+      }
+    }else
+    {
+      switch (filterOperator.ToLower())
+      {
+        case "gt": // Greater than
+        return query.Where(
+          Expression.Lambda<Func<T, bool>>(
+            Expression.GreaterThan(
+              propertySelector.Body,
+              Expression.Constant(value)),
+            propertySelector.Parameters)
+          );
+        case "lt": // Less than
+        return query.Where(
+          Expression.Lambda<Func<T, bool>>(
+            Expression.LessThan(
+              propertySelector.Body,
+              Expression.Constant(value)),
+            propertySelector.Parameters)
+        );
+        case "eq": // Equal to
+        return query.Where(
+          Expression.Lambda<Func<T, bool>>(
+            Expression.Equal(
+              propertySelector.Body,
+              Expression.Constant(value)),
+            propertySelector.Parameters)
+        );
+      }
     }
 
+    
     return query;
   }
 
-  public static IQueryable<T> FilterOrderByCreatedAtOperator<T>(this IQueryable<T> query, string filterOperator)
+  public static IQueryable<T> BaseQuery<T>(this IQueryable<T> query, QueryParameters queryParameters)
     where T : Entity
   {
-    switch (filterOperator.ToLower())
-    {
-      case "asc": // Ascending
-        query = query.OrderBy(x => x.CreatedAt);
-        break;
-      case "desc": // Descending
-        query = query.OrderByDescending(x => x.CreatedAt);
-        break;
-    }
+    query = query.FilterByOperator(queryParameters.CreatedAtOperator,x => x.CreatedAt,queryParameters.CreatedAt);
+    query = query.Skip(queryParameters.Skip ?? 0).Take(queryParameters.Take ?? 1);
 
     return query;
   }
+
 }
 
