290b834fb03304813073ec426852ef4368aab923
Eduardo Kroetz <eduardokroetz0@gmail.com>
2024-05-12 14:35:20 -0300

    Criar método GetAsync para queries customizadas

--- Hotel.Domain/Repositories/ReservationContext/ReservationRepository.cs
+++ Hotel.Domain/Repositories/ReservationContext/ReservationRepository.cs
@@ -1,7 +1,9 @@
 using Hotel.Domain.Data;
+using Hotel.Domain.DTOs.Base.User;
 using Hotel.Domain.DTOs.PaymentContext.RoomInvoiceDTOs;
-using Hotel.Domain.DTOs.User;
+using Hotel.Domain.DTOs.ReservationContext.ReservationDTOs;
 using Hotel.Domain.Entities.ReservationContext.ReservationEntity;
+using Hotel.Domain.Extensions;
 using Hotel.Domain.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
@@ -28,19 +30,61 @@ public class ReservationRepository :  GenericRepository<Reservation> ,IReservati
         x.RoomId,
         new List<GetUser>(
           x.Customers.Select(
-            c => new GetUser(c.Id,c.Name.FirstName,c.Name.LastName,c.Email.Address,c.Phone.Number)
+            c => new GetUser(c.Id,c.Name.FirstName,c.Name.LastName,c.Email.Address,c.Phone.Number,c.CreatedAt)
         )),
         x.InvoiceId,
         x.Services))
       .FirstOrDefaultAsync();
     
   }
-  public async Task<IEnumerable<GetReservationCollection>> GetAsync()
+
+  public async Task<IEnumerable<GetReservationCollection>> GetAsync(ReservationQueryParameters queryParameters)
   {
-    return await _context
-      .Reservations
-      .AsNoTracking()
-      .Select(x => new GetReservationCollection(x.Id,x.DailyRate,x.HostedDays,x.CheckIn,x.CheckOut,x.Status,x.Capacity,x.RoomId,x.InvoiceId))
-      .ToListAsync();
+    var query = _context.Reservations.AsQueryable();
+
+    if (queryParameters.HostedDays.HasValue)
+      query = query.Where(x => x.HostedDays == queryParameters.HostedDays);
+
+    if (queryParameters.DailyRate.HasValue)
+      query = query.FilterByOperator(queryParameters.DailyRateOperator, x => x.DailyRate, queryParameters.DailyRate);
+
+    if (queryParameters.CheckIn.HasValue)
+      query = query.FilterByOperator(queryParameters.CheckInOperator, x => x.CheckIn, queryParameters.CheckIn);
+
+    if (queryParameters.CheckOut.HasValue)
+      query = query.FilterByOperator(queryParameters.CheckOutOperator, x => x.CheckOut, queryParameters.CheckOut);
+
+    if (queryParameters.Status.HasValue)
+      query = query.Where(x => x.Status == queryParameters.Status);
+
+    if (queryParameters.Capacity.HasValue)
+      query = query.FilterByOperator(queryParameters.CapacityOperator, x => x.Capacity, queryParameters.Capacity);
+
+    if (queryParameters.CustomerId.HasValue)
+      query = query.Where(x => x.Customers.Any(x => x.Id == queryParameters.CustomerId));
+
+    if (queryParameters.RoomId.HasValue)
+      query = query.Where(x => x.RoomId == queryParameters.RoomId);
+
+    if (queryParameters.InvoiceId.HasValue)
+      query = query.Where(x => x.InvoiceId == queryParameters.InvoiceId);
+
+    if (queryParameters.ServiceId.HasValue)
+      query = query.Where(x => x.Services.Any(x => x.Id == queryParameters.ServiceId));
+
+
+    query = query.BaseQuery(queryParameters);
+
+    return await query.Select(x => new GetReservationCollection(
+        x.Id,
+        x.DailyRate,
+        x.HostedDays,
+        x.CheckIn,
+        x.CheckOut,
+        x.Status,
+        x.Capacity,
+        x.RoomId,
+        x.InvoiceId
+    )).ToListAsync();
   }
 }
