4c0befaab1ab623173d69521c6dc8d5af2533c83
Eduardo Kroetz <eduardokroetz0@gmail.com>
2024-05-12 14:19:56 -0300

    Criar método GetAsync para query personalizada

--- Hotel.Domain/Repositories/PaymentContext/RoomInvoiceRepository.cs
+++ Hotel.Domain/Repositories/PaymentContext/RoomInvoiceRepository.cs
@@ -1,6 +1,7 @@
 using Hotel.Domain.Data;
 using Hotel.Domain.DTOs.PaymentContext.RoomInvoiceDTOs;
 using Hotel.Domain.Entities.PaymentContext.InvoiceRoomEntity;
+using Hotel.Domain.Extensions;
 using Hotel.Domain.Repositories.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
@@ -21,12 +22,48 @@ public class RoomInvoiceRepository : GenericRepository<RoomInvoice> ,IRoomInvoic
       .FirstOrDefaultAsync();
     
   }
-  public async Task<IEnumerable<GetRoomInvoice>> GetAsync()
+
+  public async Task<IEnumerable<GetRoomInvoice>> GetAsync(RoomInvoiceQueryParameters queryParameters)
   {
-    return await _context
-      .RoomInvoices
-      .AsNoTracking()
-      .Select(x => new GetRoomInvoice(x.Number,x.PaymentMethod,x.ReservationId,x.IssueDate,x.TotalAmount,x.Status,x.TaxInformation))
-      .ToListAsync();
+    var query = _context.RoomInvoices.AsQueryable();
+
+    if (queryParameters.Number != null)
+      query = query.Where(x => x.Number.Contains(queryParameters.Number));
+
+    if (queryParameters.IssueDate.HasValue)
+      query = query.FilterByOperator(queryParameters.IssueDateOperator,x => x.IssueDate,queryParameters.IssueDate);
+
+    if (queryParameters.TotalAmount.HasValue)
+      query = query.FilterByOperator(queryParameters.TotalAmountOperator, x => x.TotalAmount, queryParameters.TotalAmount);
+
+    if (queryParameters.Status.HasValue)
+      query = query.Where(x => x.Status == queryParameters.Status);
+
+    if (queryParameters.PaymentMethod.HasValue)
+      query = query.Where(x => x.PaymentMethod == queryParameters.PaymentMethod);
+
+    if (queryParameters.CustomerId.HasValue)
+      query = query.Where(x => x.Customers.Any(x => x.Id == queryParameters.CustomerId));
+
+    if (queryParameters.ReservationId.HasValue)
+      query = query.Where(x => x.ReservationId == queryParameters.ReservationId);
+
+    if (queryParameters.ServiceId.HasValue)
+      query = query.Where(x => x.Services.Any(x => x.Id == queryParameters.ServiceId));
+
+    if (queryParameters.TaxInformation.HasValue)
+      query = query.FilterByOperator(queryParameters.TaxInformationOperator, x => x.TaxInformation, queryParameters.TaxInformation);
+
+    query.BaseQuery(queryParameters);
+
+    return await query.Select(x => new GetRoomInvoice(
+      x.Number,
+      x.PaymentMethod,
+      x.ReservationId,
+      x.IssueDate,
+      x.TotalAmount,
+      x.Status,
+      x.TaxInformation
+    )).ToListAsync();
   }
 }
