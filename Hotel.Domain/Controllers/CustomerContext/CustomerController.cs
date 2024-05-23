43514ee8b3cc0244d199fc6594ea3de5372a844f
Eduardo Kroetz <eduardokroetz0@gmail.com>
2024-05-23 08:48:39 -0300

    Criar funcionalidades para atualizar os campos de um cliente -Campos que podem ser atualizados: Nome,Email,Telefone, Endereço, Gênero e Data de Nascimento

--- Hotel.Domain/Controllers/CustomerContext/CustomerController.cs
+++ Hotel.Domain/Controllers/CustomerContext/CustomerController.cs
@@ -1,5 +1,7 @@
 using Hotel.Domain.DTOs.Base.User;
+using Hotel.Domain.Enums;
 using Hotel.Domain.Handlers.CustomerContext.CustomerHandlers;
+using Hotel.Domain.ValueObjects;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Hotel.Domain.Controllers.CustomerContext;
@@ -44,5 +46,48 @@ public class CustomerController : ControllerBase
     [FromRoute]Guid id
   )
   => Ok(await _handler.HandleDeleteAsync(id));
-  
+
+  [HttpPatch("v1/customers/{customerId:guid}/name")]
+  public async Task<IActionResult> UpdateNameAsync(
+    [FromRoute] Guid customerId,
+    [FromBody] Name name
+  )
+  => Ok(await _handler.HandleUpdateNameAsync(customerId, name));
+
+  [HttpPatch("v1/customers/{customerId:guid}/email")]
+  public async Task<IActionResult> UpdateEmailAsync(
+    [FromRoute] Guid customerId,
+    [FromBody] Email email
+  )
+  => Ok(await _handler.HandleUpdateEmailAsync(customerId, email));
+
+  [HttpPatch("v1/customers/{customerId:guid}/phone")]
+  public async Task<IActionResult> UpdatePhoneAsync(
+    [FromRoute] Guid customerId,
+    [FromBody] Phone phone
+  )
+  => Ok(await _handler.HandleUpdatePhoneAsync(customerId, phone));
+
+  [HttpPatch("v1/customers/{customerId:guid}/address")]
+  public async Task<IActionResult> UpdateAddressAsync(
+    [FromRoute] Guid customerId,
+    [FromBody] Address address
+  )
+  => Ok(await _handler.HandleUpdateAddressAsync(customerId, address));
+
+
+  [HttpPatch("v1/customers/{customerId:guid}/gender/{gender:int}")]
+  public async Task<IActionResult> UpdateGenderAsync(
+    [FromRoute] Guid customerId,
+    [FromRoute] int gender
+  )
+  => Ok(await _handler.HandleUpdateGenderAsync(customerId, (EGender)gender));
+
+  [HttpPatch("v1/customers/{customerId:guid}/date-of-birth")]
+  public async Task<IActionResult> UpdateDateOfBirthAsync(
+    [FromRoute] Guid customerId,
+    [FromBody] UpdateDateOfBirth newDateOfBirth
+  )
+  => Ok(await _handler.HandleUpdateDateOfBirthAsync(customerId, newDateOfBirth.DateOfBirth));
+
 }
