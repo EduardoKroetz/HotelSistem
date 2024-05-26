cb1a1b62f0fda5cd21b93b45ad86c62a6c7f20f3
Eduardo Kroetz <eduardokroetz0@gmail.com>
2024-05-26 14:24:54 -0300

    refactoring: organizar os arquivos de inicialização na pasta 'Initialization'

---
+++ Hotel.Domain/Initialization/ConfigureAuthentication.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Hotel.Domain.Initialization;
+
+public static class ConfigureAuthentication
+{
+  public static void Configure(IServiceCollection services)
+  {
+    var key = Encoding.ASCII.GetBytes(Configuration.Configuration.JwtKey);
+    services.AddAuthentication(options =>
+    {
+      options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+      options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+      options.RequireHttpsMetadata = true; // Garantir HTTPS
+      options.SaveToken = true; // Salvar o token no contexto de autenticação
+
+      options.TokenValidationParameters = new TokenValidationParameters()
+      {
+        ValidateIssuerSigningKey = true, // Validar a chave de assinatura
+        IssuerSigningKey = new SymmetricSecurityKey(key), // Chave de assinatura simétrica
+        ValidateAudience = false,
+        ValidateIssuer = false
+      };
+    });
+  }
+}
