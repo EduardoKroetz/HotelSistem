947782c4b80340f51a898b3785f191059a55ca48
Eduardo Kroetz <eduardokroetz0@gmail.com>
2024-06-18 10:38:54 -0300

    enhancement: interface de serviço do stripe(IStripeService)

---
+++ Hotel.Domain/Services/Interfaces/IStripeService.cs
@@ -0,0 +1,30 @@
+﻿using Stripe;
+
+namespace Hotel.Domain.Services.Interfaces;
+
+public interface IStripeService
+{
+    // CRUD de Customer
+    Task<Customer> CreateCustomerAsync(string name, string email);
+    Task<Customer> GetCustomerAsync(string customerId);
+    Task<Customer> UpdateCustomerAsync(string customerId, string name, string email);
+    Task<bool> DeleteCustomerAsync(string customerId);
+
+    // CRUD de Room (produto no Stripe)
+    Task<Product> CreateRoomAsync(string name, string description);
+    Task<Product> GetRoomAsync(string productId);
+    Task<Product> UpdateRoomAsync(string productId, string name, string description);
+    Task<bool> DeleteRoomAsync(string productId);
+
+    // CRUD de Service (produto no Stripe)
+    Task<Product> CreateServiceAsync(string name, string description);
+    Task<Product> GetServiceAsync(string productId);
+    Task<Product> UpdateServiceAsync(string productId, string name, string description);
+    Task<bool> DeleteServiceAsync(string productId);
+
+    // CRUD de Reservation (PaymentIntent no Stripe)
+    Task<PaymentIntent> CreateReservationAsync(string customerId, string roomId, int amount, string currency);
+    Task<PaymentIntent> GetReservationAsync(string paymentIntentId);
+    Task<PaymentIntent> UpdateReservationAsync(string paymentIntentId, int amount, string currency);
+    Task<bool> CancelReservationAsync(string paymentIntentId);
+}
---
+++ Hotel.Domain/Services/StripeService.cs
@@ -0,0 +1,102 @@
+﻿using Hotel.Domain.Services.Interfaces;
+using Stripe;
+
+namespace Hotel.Domain.Services;
+
+public class StripeService : IStripeService
+{
+    //Customer
+    public async Task<Customer> CreateCustomerAsync(string name, string email)
+    {
+        var options = new CustomerCreateOptions 
+        { 
+            Name = name, 
+            Email = email 
+        };
+
+        var service = new CustomerService();
+        return await service.CreateAsync(options);  
+    }
+
+    public Task<bool> DeleteCustomerAsync(string customerId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Customer> GetCustomerAsync(string customerId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Customer> UpdateCustomerAsync(string customerId, string name, string email)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    //Room
+    public Task<Product> CreateRoomAsync(string name, string description)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteRoomAsync(string productId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product> GetRoomAsync(string productId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product> UpdateRoomAsync(string productId, string name, string description)
+    {
+        throw new NotImplementedException();
+    }
+
+    //Service
+    public Task<Product> CreateServiceAsync(string name, string description)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteServiceAsync(string productId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product> GetServiceAsync(string productId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product> UpdateServiceAsync(string productId, string name, string description)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    //Reservation
+    public Task<PaymentIntent> CreateReservationAsync(string customerId, string roomId, int amount, string currency)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> CancelReservationAsync(string paymentIntentId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<PaymentIntent> GetReservationAsync(string paymentIntentId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<PaymentIntent> UpdateReservationAsync(string paymentIntentId, int amount, string currency)
+    {
+        throw new NotImplementedException();
+    }
+
+
+}
